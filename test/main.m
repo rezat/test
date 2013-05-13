c7609bd6d8663e5c22fd0ffaaaf237446aa4b2f1
nikita <smyseab@SMYSE-ABs-iMac.local>
2013-05-13 16:08:55 +0200

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by nikita on 5/13/13.
+//  Copyright (c) 2013 nikita. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
